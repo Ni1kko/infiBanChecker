8e407404f9444a3553c6127bd3bcace129298817
(Fallox) Nikko Renolds <ni1kko@outlook.com>
2019-10-01 15:14:12 +0100

    clean up whitespace & unused things

--- infiBanChecker/infiBanChecker/Program.cs
+++ infiBanChecker/infiBanChecker/Program.cs
@@ -1,37 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Text;
-using System.Threading.Tasks;
 using static Newtonsoft.Json.Linq.JObject;
 
 namespace infiBanChecker
-{
-    
+{ 
     class Program
     {
         private protected const string endPoint = "https://api.infistar.de/arma/getGlobalBan"; 
         private protected static string steamID;
         private protected static string infiToken;
         public static string APIErrorMessage;
-        static void Main(string[] args)
+        static void Main()
         {
             steamID = "76561198050103064";
             infiToken = "someTokenHere"; 
             string uri = $"{endPoint}?license_token={infiToken}&uid={steamID}";
-
-            //Console.WriteLine(uri);
-            //Console.ReadKey();
-             
+  
             HttpClient client = new HttpClient();
             client.BaseAddress = new Uri(endPoint); 
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json")); 
             HttpResponseMessage APIresponse = client.GetAsync(uri).Result; 
             dynamic data = Parse(APIresponse.Content.ReadAsStringAsync().Result);
      
-            
             if (APIresponse.IsSuccessStatusCode)
             { 
                 if (data.status == "success")
@@ -40,7 +31,6 @@ namespace infiBanChecker
                     Console.WriteLine($"Found Result For SteamID: {data.message.uid}\r\n");
                     Console.WriteLine(
                         $"{((data.message.state == "1") ? ("GlobalBan | " + data.message.bandate + "\r\n") : "Clean\r\n")}");
-
                 } 
             }
             else
