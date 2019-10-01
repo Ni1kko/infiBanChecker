9170d919180174c54275cd8c1ad74d3982f7ca8c
(Fallox) Nikko Renolds <ni1kko@outlook.com>
2019-10-01 15:44:41 +0100

    get token from local json config file

--- infiBanChecker/infiBanChecker/Program.cs
+++ infiBanChecker/infiBanChecker/Program.cs
@@ -1,13 +1,18 @@
 ﻿using System;
+using System.IO;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using static Newtonsoft.Json.Linq.JObject;
+using System.Net.Mime;
+using System.Threading;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq; 
 
 namespace infiBanChecker
 { 
     class Program
-    {
-        private protected const string endPoint = "https://api.infistar.de/arma/getGlobalBan"; 
+    { 
+        private protected const string endPoint = "https://api.infistar.de/arma/getGlobalBan";
+        private protected static string config = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "config.json");
         private protected static string steamID;
         private protected static string infiToken;
         public static string APIErrorMessage;
@@ -21,18 +26,44 @@ namespace infiBanChecker
                 return Console.ReadLine();
             }
         }
+        private static object getJsonValue(string token)
+        { 
+            var jRead = File.OpenText(config);
+            var jreader = new JsonTextReader(jRead);
+            var jfile = (JObject)JToken.ReadFrom(jreader);
+             
+            if (!jfile.ContainsKey(token))
+            {
+                Console.WriteLine($"Unable to find Json Token:( '{token}' )");
+                Console.Beep(1300, 200); 
+                Console.WriteLine("Press `ANY Key` To Exit");
+                Console.ReadKey(); 
+                Environment.Exit(0);
+            } 
+
+            return jfile.GetValue(token).ToString();
+        }
 
         static void Main()
         {
+            if (!File.Exists(config))
+            {
+                Console.WriteLine($"Unable to find file:( {config} )");
+                Console.Beep(3700, 500);//Im soo fucking sorry :crying-with-laughter: 
+                Console.WriteLine("Exiting...");
+                Thread.Sleep(400);
+                Environment.Exit(0);
+            }
+
             steamID = getID; 
-            infiToken = "someTokenHere"; 
+            infiToken = (string)getJsonValue("infiStarLic"); 
             string uri = $"{endPoint}?license_token={infiToken}&uid={steamID}";
   
             HttpClient client = new HttpClient();
             client.BaseAddress = new Uri(endPoint); 
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json")); 
             HttpResponseMessage APIresponse = client.GetAsync(uri).Result; 
-            dynamic data = Parse(APIresponse.Content.ReadAsStringAsync().Result);
+            dynamic data = JObject.Parse(APIresponse.Content.ReadAsStringAsync().Result);
      
             if (APIresponse.IsSuccessStatusCode)
             { 
